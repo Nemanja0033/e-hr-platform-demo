84c36d12eff2a6417dd19451043cde9527d79ff4
Nemanja Antonijevic <antonijevicnemanjaa@gmail.com>
2026-02-20 21:04:35 +0100

    feat: refactor auth guard to use synchronous user authentication and update navbar logic

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,6 +1,5 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { map } from 'rxjs/operators';
 import { UserStore } from '../store/user.store';
 
 export const authGuard = (role: "hr" | "employe"): CanActivateFn => {
@@ -8,14 +7,14 @@ export const authGuard = (role: "hr" | "employe"): CanActivateFn => {
   return () => {
     const userStore = inject(UserStore);
     const router = inject(Router);
-    return userStore.isUserAuth$.pipe(
-      map(isAuth => {
-        if (isAuth) {
-          return true;
-        }
 
-        return router.createUrlTree([`/${role}/auth`]);
-      })
-    );
+    const isAuth = userStore.isUserAuth();
+
+    if (isAuth) {
+      return true;
+    }
+    else {
+      return router.createUrlTree([`/${role}/auth`]);
+    }
   }
 };
