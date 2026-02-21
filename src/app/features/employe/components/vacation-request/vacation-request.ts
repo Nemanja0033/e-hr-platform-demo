5e72204d8315d1163b5d2bd8b15bc92b5bea67e2
Nemanja Antonijevic <antonijevicnemanja68@gmail.com>
2026-02-21 03:50:24 +0100

    vacation component logic

--- src/app/features/employe/components/vacation-request/vacation-request.ts
+++ src/app/features/employe/components/vacation-request/vacation-request.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { UserStore } from '../../../../core/store/user.store';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-vacation-request',
@@ -13,29 +14,53 @@ import { UserStore } from '../../../../core/store/user.store';
 })
 export class VacationRequest {
   isRequestMode = signal(false);
+  isInvalidDateRange = signal<boolean | null>(null);
   vacationDateForm;
   employeeData;
+  requestedVacationDays = signal<number | null>(null);
 
-  constructor(private fb: FormBuilder, private userStore: UserStore){
+  constructor(
+    private fb: FormBuilder,
+    private userStore: UserStore,
+    private _snackbar: MatSnackBar
+  ) {
     this.employeeData = userStore.user;
-
     this.vacationDateForm = this.fb.nonNullable.group({
       startDate: [new Date(), Validators.required],
-      endDate: [new Date(new Date().setDate(new Date().getDate() + 20)), Validators.required]
-    })
+      endDate: [new Date(), Validators.required],
+    });
   }
 
-  toggleRequestMode(){
+  ngOnInit() {
+    this.vacationDateForm.valueChanges.subscribe((val) => {
+      if (val.startDate && val.endDate && val.endDate > new Date()) {
+        const diff = Math.ceil(
+          (new Date(val.endDate).getTime() - new Date(val.startDate).getTime()) /
+            (1000 * 60 * 60 * 24)
+        );
+        this.requestedVacationDays.set(diff);
+        const daysLeft = this.employeeData()?.vacationDays ?? 0;
+
+        if (diff > daysLeft) {
+          this._snackbar.open('You dont have required amount of vacation days.');
+          this.isInvalidDateRange.set(true);
+        }
+        else{
+          this.isInvalidDateRange.set(false);
+        }
+      }
+    });
+  }
+
+  toggleRequestMode() {
     this.isRequestMode.set(true);
   }
 
-  onRequestFormSubmit(){
-    if(this.vacationDateForm.invalid){
+  onRequestFormSubmit() {
+    if (this.vacationDateForm.invalid) {
       this.vacationDateForm.markAllAsTouched();
-      console.log('invalid')
       return;
     }
-    
-    console.log("FORM DATA", this.vacationDateForm.getRawValue());
+
   }
 }
